e6b9b70eb0ef34a72d263ae5f115f3b5efd6debc
karinEkenberg <zirkon6@hotmail.com>
2024-01-09 12:22:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Diagnostics;//is needed
+using Microsoft.Extensions.Configuration;//for packages
+
+namespace InstrumentingExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string logPaths = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+            Console.WriteLine($"Writing to: {logPaths}.");
+            TextWriterTraceListener logFile = new (File.CreateText(logPaths));
+            Trace.Listeners.Add(logFile);
+            Trace.AutoFlush = true;
+            Debug.WriteLine("Debyg: I am looking..");
+            Trace.WriteLine("Trace: I am looking..");
+            Console.WriteLine($"Reading from appsettings.json in {0}", arg0: Directory.GetCurrentDirectory());
+            ConfigurationBuilder builder = new();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            TraceSwitch ts = new(
+                displayName: "MinSwitch",
+                description: "This switch is set via a JSON config");
+            configuration.GetSection("MinSwitch").Bind(ts);
+            Trace.WriteLine(ts.TraceError, "Trace errors..");
+            Trace.WriteLine(ts.TraceWarning, "Trace warnings..");
+            Trace.WriteLine(ts.TraceInfo, "Trace information..");
+            Trace.WriteLine(ts.TraceVerbose, "Trace verboses..");
+            Console.ReadLine();
+        }
+    }
+}
